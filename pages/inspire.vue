5b0c33a515d58dd2f5ce319f4fa83fa38b409428
gurumee92 <gurumee92@gmail.com>
2021-09-19 19:34:10 +0900

    Update pages

--- pages/inspire.vue
+++ pages/inspire.vue
@@ -1,19 +1,9 @@
 <template>
-  <v-row>
-    <v-col class="text-center">
-      <img
-        src="/v.png"
-        alt="Vuetify.js"
-        class="mb-5"
-      >
-      <blockquote class="blockquote">
-        &#8220;First, solve the problem. Then, write the code.&#8221;
-        <footer>
-          <small>
-            <em>&mdash;John Johnson</em>
-          </small>
-        </footer>
-      </blockquote>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <v-card class="logo py-4 d-flex justify-center">
+        인스파이어 페이지
+      </v-card>
     </v-col>
   </v-row>
 </template>
