c98ae3faed38321b9145b576ea93b048a20d7748
gurumee92 <gurumee92@users.noreply.github.com>
2021-09-19 10:32:13 +0000

    deploy: c202ec5d03dc8454454026ea3780548df233824f

---
+++ _nuxt/static/1632047531/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fportfolio\u002F_nuxt\u002Fstatic\u002F1632047531",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fportfolio\u002F",assetsPath:"\u002Fportfolio\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"207e691a"}}}}(null));
